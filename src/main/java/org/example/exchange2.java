b7dc856a2a8eb3d9dec4d27961721c59c8302a1c
Andrey Sanzharov <83120731+AndreySanzharov@users.noreply.github.com>
2025-06-18 13:15:15 +0300

    Update exchange2.java

--- src/main/java/org/example/exchange2.java
+++ src/main/java/org/example/exchange2.java
@@ -1,62 +1,88 @@
-package org.example.TagBot;
+import com.bmc.arsys.api.*;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class ARSMethods {
+    public static String createIncident(String submitter, String status, String description, String information, String firstName, String lastName, String middleName, String castCompany, String category) throws ARException {
+        //String formName = "HPD:Help Desk";
+        String formName = "HPD:IncidentInterface_Create";
+        String entryIdOut;
+        String incNumder;
+        // try {
+        Entry entry = new Entry();
+        entry.put(Constants.AR_CORE_SUBMITTER, new Value(submitter));
+        entry.put(Constants.AR_CORE_STATUS, new Value(status, DataType.ENUM));
+        //entry.put(Constants.AR_CORE_SHORT_DESCRIPTION, new Value());
+        entry.put(1000000076, new Value("CREATE")); //action
+        entry.put(1000000019, new Value(firstName)); //first_name
+        entry.put(1000000018, new Value(lastName)); //last_name
+        entry.put(1000000020, new Value(middleName)); //middle name
+        entry.put(1000000082, new Value(castCompany)); //company
+        entry.put(1000000163, new Value("3000", DataType.ENUM)); //impact
+        entry.put(1000000162, new Value("3000", DataType.ENUM)); //urgency
+        entry.put(1000000000, new Value(description)); //description
+        entry.put(1000000151, new Value(information)); //detailed description
+        entry.put(1000000215, new Value("10000", DataType.ENUM)); //reported source
 
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+        int choice = 1;
 
-public class Handlers {
-    private static final Logger log = LoggerFactory.getLogger(Handlers.class);
-    private static UserStateManager userStateManager;
+        if (!category.equals("n/a")) {
+            switch (category) {
 
-    // Пул потоков для выполнения задач
-    private static final ExecutorService executor = Executors.newCachedThreadPool();
+                case "Инцидент": {
+                    choice = 0;
+                    break;
+                }
+                case "Инцидент мониторинга": {
+                    choice = 2;
+                    break;
+                }
+                case "Событие мониторинга": {
+                    choice = 3;
+                    break;
+                }
+                case "Жалоба": {
+                    choice = 4;
+                    break;
+                }
+                default: {
 
-    public static void setUserStateManager(UserStateManager manager) {
-        userStateManager = manager;
-    }
-
-    public static Future<Boolean> validateNameAsync(String input, String chatId) {
-        return executor.submit(() -> {
-            log.info("Сработал обработчик validateName");
-            if (input == null) return false;
-            int spaceCount = (int) input.chars().filter(ch -> ch == ' ').count();
-            boolean valid = spaceCount >= 2;
-
-            if (!valid) {
-                userStateManager.sendText(chatId, "Неверный формат ФИО. Попробуйте еще раз");
+                }
             }
-            log.info("Проверка ФИО '{}': {} пробелов => {}", input, spaceCount, valid);
-            return valid;
-        });
-    }
+        }
+        entry.put(1000000099, new Value(choice, DataType.ENUM)); //service type
+        return formName;
 
-    public static Future<Boolean> searchNameAsync(String input, String chatId) {
-        return executor.submit(() -> {
-            log.info("Сработал обработчик searchName");
-            Set<String> usersList = Set.of("Иванов Иван Иванович", "Петров Петр Петрович"); // пример
-            boolean isUserInList = usersList.contains(input);
-            if (!isUserInList) {
-                userStateManager.sendText(chatId, "Пользователь не найден. Попробуйте еще раз");
-            }
-            return isUserInList;
-        });
-    }
 
-    public static Future<Boolean> answersAsync(String input, String chatId) {
-        return executor.submit(() -> {
-            log.info("Сработал обработчик answers");
-            String question = "??";
-            // здесь можно добавить логику обработки ответа
-            return true;
-        });
-    }
+        entryIdOut = server.createEntry(formName, entry);
 
-    // Метод для завершения всех потоков — вызывать при остановке приложения
-    public static void shutdown() {
-        executor.shutdown();
+        Entry incEntry = server.getEntry("HPD:IncidentInterface_Create", entryIdOut,null);
+        Value incVal = incEntry.get(1000000161);
+        incNumder = incVal.toString();
     }
 }
+
+import com.bmc.arsys.api.*;
+
+public class ARSApplication {
+
+
+    private static final String SERVER = "iss2tstars01";
+    private static final String NAME = "sanzharovaa";
+    private static final String PASSWORD = "123";
+    //private static ARSMethods arsMethods = new ARSMethods();
+
+    public static void main(String[] args) throws ARException {
+        connect(SERVER, NAME, PASSWORD);
+        ARSMethods.createIncident("7", "7", "desc", "info", "AA", "SS", "AA", "lukoil", "Инцидент");
+    }
+
+    public static void connect(String serverName, String userName, String userPassword) throws ARException {
+        ARServerUser server = new ARServerUser();
+        server.setPort(35000);
+        server.setServer(serverName);
+        server.setUser(userName);
+        server.setPassword(userPassword);
+        server.verifyUser();
+    }
+}
+
+
